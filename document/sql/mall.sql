ae8d4a8a6161d27a4d34d7416743b42915835f22
sunyuhu0301 <1351089670@qq.com>
2020-07-27 14:35:51 +0800

    mall 配置改成连接119.45.139.62服务器

--- document/sql/mall.sql
+++ document/sql/mall.sql
@@ -3,7 +3,7 @@ Navicat MySQL Data Transfer
 
 Source Server         : localhost
 Source Server Version : 50719
-Source Host           : localhost:3306
+Source Host           : 119.45.139.62
 Source Database       : mall
 
 Target Server Type    : MYSQL
